5e358e754dc6e89e430d447ae5dadd38a113f0eb
Ardintsev Sergey <archunter1369@gmail.com>
2022-11-05 13:30:52 +0300

    router fix

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import { Recipe } from './pages/Recipe';
 function App() {
   return (
     <>
-      <Router>
+      <Router basename='/react-foods'>
         <Header />
         <main className='container content'>
           <Routes>
